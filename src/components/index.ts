da020ee917c3e089bbf8387cab659dde99802721
Shubham-ly <shubham.m@ly.design>
2023-04-06 17:32:51 +0530

    Stat panel container Device status Tooltip

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,17 @@
 import SingleSelectDropdown from './dropdown/SingleSelectDropdown';
 import MultiSelectDropdown from './dropdown/MultiSelectDropdown';
 import Button from './button/Button';
-export { SingleSelectDropdown, MultiSelectDropdown, Button };
+import ToggleButton from './toggle-button/ToggleButton';
+import StatPanelContainer from './panels/StatPanelContainer';
+import Status from './panels/Status';
+import DeviceStatus from './panels/DeviceStatus';
+
+export {
+  SingleSelectDropdown,
+  MultiSelectDropdown,
+  Button,
+  ToggleButton,
+  StatPanelContainer,
+  Status,
+  DeviceStatus,
+};
---
+++ src/global/index.ts
@@ -0,0 +1,11 @@
+export enum Criticality {
+  SAFE = '#00BF54',
+  MEDIUM = '#FAC848',
+  MAJOR = '#E17538',
+  CRITICAL = '#ED3445',
+}
+
+export enum DeviceReachability {
+  REACHABLE = 'REACHABLE',
+  UNREACHABLE = 'UNREACHABLE',
+}
