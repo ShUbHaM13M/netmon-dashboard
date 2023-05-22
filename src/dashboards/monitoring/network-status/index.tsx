464741d342d67b11a9eefa1589479a740f9203ee
Shubham-ly <shubham.m@ly.design>
2023-05-22 11:02:45 +0530

    Network issues details dashboard

--- src/dashboards/monitoring/network-status/index.tsx
+++ src/dashboards/monitoring/network-status/index.tsx
@@ -1,17 +1,38 @@
 import { StatPanelContainer, Table } from '../../../components';
 import Like from '../../../assets/images/like.svg';
+import { API_URL, FetchPanelData, dateFormatter, headers } from '../../../global';
+import { useUserContext } from '../../../context/UserContext';
+import useFetch from '../../../hooks/useFetch';
 
 const NetworkStatus = () => {
+  const { refetch, timestamp } = useUserContext();
+
+  const rebootSummaryDataURL = `${API_URL}/panel/network/reboot/summary?from=${timestamp.from.getTime()}&to=${timestamp.to.getTime()}&ver=v2`;
+  const { data: rebootSummaryData } = useFetch<FetchPanelData>(
+    rebootSummaryDataURL,
+    { headers },
+    refetch,
+  );
+
+  const rebootDetailsDataURL = `${API_URL}/panel/network/reboot/details?from=${timestamp.from.getTime()}&to=${timestamp.to.getTime()}&ver=v2`;
+  const { data: rebootDetailsData } = useFetch<FetchPanelData>(
+    rebootDetailsDataURL,
+    { headers },
+    refetch,
+  );
+
   return (
     <div className='flex flex-col lg:flex-row pb-6 gap-6 h-full text-icon-white mt-4'>
       <div className='h-[410px] lg:h-[510px] w-full'>
         <StatPanelContainer
-          label='Reboot Summary from -- to --'
+          label={`Reboot Summary from ${dateFormatter.format(
+            timestamp.from,
+          )} to ${dateFormatter.format(timestamp.to)}`}
           description='Showing the reboot summary'
         >
           <Table
-            headers={[]}
-            data={[]}
+            headers={rebootSummaryData ? rebootSummaryData.columns : []}
+            data={rebootSummaryData ? rebootSummaryData.data : []}
             emptyStateData={{
               icon: Like,
               title: 'No Reboot Summary',
@@ -26,8 +47,8 @@ const NetworkStatus = () => {
           description='This panel shows the Reboot details'
         >
           <Table
-            headers={[]}
-            data={[]}
+            headers={rebootDetailsData?.columns || []}
+            data={rebootDetailsData?.data || []}
             emptyStateData={{
               icon: Like,
               title: 'No Reboot Details',
