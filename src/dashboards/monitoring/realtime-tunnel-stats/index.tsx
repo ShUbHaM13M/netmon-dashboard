41eeee13a59c9b71e0358875ffed7a169c0996fe
Shubham-ly <shubham.m@ly.design>
2023-05-22 13:07:38 +0530

    Added Monitoring/RealtimeTunnelStats dashboard

--- src/dashboards/monitoring/realtime-tunnel-stats/index.tsx
+++ src/dashboards/monitoring/realtime-tunnel-stats/index.tsx
@@ -1,28 +1,45 @@
 import { useEffect, useState } from 'react';
-import { API_URL, headers, FetchData } from '../../../global';
+import { API_URL, headers, FetchData, FetchPanelData } from '../../../global';
 import useFetch from '../../../hooks/useFetch';
 import { SingleSelectDropdown, StatPanelContainer, Table } from '../../../components';
 import Like from '../../../assets/images/like.svg';
+import { useUserContext } from '../../../context/UserContext';
 
 const siteOptionURL = `${API_URL}/vars?name=site&filter-name=site-name&filter-value=All`;
-const deviceOptionURL = `${API_URL}/vars?name=device&filter-name=site-name&filter-value=All`;
 
 const RealtimeTunnelStats = () => {
+  const { refetch } = useUserContext();
   const { data: siteOptions } = useFetch<FetchData[]>(siteOptionURL, {
     headers,
   });
 
-  const { data: deviceOptions } = useFetch<FetchData[]>(deviceOptionURL, { headers });
-
   const [selectedSite, setSelectedSite] = useState<FetchData>();
+
+  const deviceOptionURL = `${API_URL}/vars?name=device&filter-name=site-name&filter-value=${selectedSite?.Text}`;
+  const { data: deviceOptions } = useFetch<FetchData[]>(deviceOptionURL, { headers }, false);
+
   const [selectedDevice, setSelectedDevice] = useState<FetchData>();
 
+  const realtimeTunnelStatsURL = `${API_URL}/tunnel/stats?device-id=${selectedDevice?.Value}&ver=v2`;
+  const { data: realtimeTunnelStatsData } = useFetch<FetchPanelData>(
+    realtimeTunnelStatsURL,
+    {
+      headers,
+    },
+    refetch,
+    !selectedDevice,
+  );
+
   useEffect(() => {
-    if (siteOptions) setSelectedSite(siteOptions[0]);
+    if (siteOptions) {
+      setSelectedSite(siteOptions[0]);
+    }
   }, [siteOptions]);
 
   useEffect(() => {
-    if (deviceOptions) setSelectedSite(deviceOptions[0]);
+    if (deviceOptions) {
+      setSelectedDevice(deviceOptions[0]);
+    }
   }, [deviceOptions]);
 
   return (
@@ -49,12 +66,12 @@ const RealtimeTunnelStats = () => {
           description='This panel shows the Realtime tunnel stats'
         >
           <Table
-            headers={[]}
-            data={[]}
+            headers={realtimeTunnelStatsData?.columns || []}
+            data={realtimeTunnelStatsData?.data || []}
             emptyStateData={{
               icon: Like,
-              title: 'All good here',
-              subtitle: 'No link down',
+              title: 'Tunnel Stats not found',
+              subtitle: 'Tunnel Stats for the device was not found',
             }}
           />
         </StatPanelContainer>
